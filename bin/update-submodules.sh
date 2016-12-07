325b3fdbfcfc27f081b1cf78377a315b738d76a9
lichuanjun <lichuanjun@niwodai.net>
2016-12-07 16:11:03 +0800

    添加自动更新submodule的快捷脚本^_^

---
+++ bin/update-submodules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+grep path .gitmodules | awk '{ print $3 }' > /tmp/study-git-submodule-dirs
+
+# read
+while read LINE
+do
+    echo $LINE
+    (cd ./$LINE && git checkout master && git pull)
+done < /tmp/study-git-submodule-dirs
+
+
